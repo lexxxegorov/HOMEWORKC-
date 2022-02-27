c5cb2d1f042ab8d481e05b898fb203b899a468e7
Aleksey <egorov.axl@gmail.com>
2022-02-27 14:28:59 +0300

    Task 6 done

---
+++ Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿int number = new int();
+
+Console.Write("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number%2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+else
+{
+    Console.WriteLine("Нечетное");
+}
