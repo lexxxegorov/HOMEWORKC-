3dea00921145e552b495d4b7c5b2d4995680dd22
Aleksey <egorov.axl@gmail.com>
2022-02-27 15:30:35 +0300

    Task 8 done

---
+++ Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = new int();
+Console.Write("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+int n = 1;
+
+for (n = 1; n <= number; n++)      
+if (n%2==0)            
+Console.WriteLine(n);
+
+
