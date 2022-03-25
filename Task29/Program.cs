4838ebab3e7b9cae6c3d9e494d675555110eeb4c
Aleksey <egorov.axl@gmail.com>
2022-03-25 20:29:15 +0300

    Add Task 29

---
+++ Task29/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N(-100 100) рандомных элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void PrintArray(int[] arrayToPrint)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write(arrayToPrint[i]);
+        if (i < arrayToPrint.Length - 1)
+        {
+           Console.Write(", "); 
+        }
+    }
+    Console.Write("]");
+    System.Console.WriteLine();
+}
+
+
+int[] createRandomArray(int length)
+{
+    int[] returningArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        returningArray[i] = new Random().Next(-100, 100);
+    }
+    return returningArray;
+}
+
+
+int[] arrayNumberOne = createRandomArray(5);
+int[] arrayNumberTwo = createRandomArray(3);
+
+PrintArray(arrayNumberOne);
+PrintArray(arrayNumberTwo);
